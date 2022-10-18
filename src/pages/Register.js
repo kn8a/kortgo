c5d9f796de20237bfa88dd472d83c52aa8a8e204
kn8a <88045655+kn8a@users.noreply.github.com>
2022-10-18 18:53:33 +0700

    registration works

--- src/pages/Register.js
+++ src/pages/Register.js
@@ -16,9 +16,68 @@ import {
   } from '@chakra-ui/react';
   import { useState } from 'react';
   import { ViewIcon, ViewOffIcon } from '@chakra-ui/icons';
+  import axios from 'axios';
+  import { useNavigate } from 'react-router';
   
   export default function Register() {
+
+    const navigate = useNavigate()
+
+    const registerURL = `${process.env.REACT_APP_API_URL}/users/register`
+
+    const [registerInfo, setRegisterInfo] = useState({
+      name_first: "",
+      name_last: "",
+      email: "",
+      password: "",
+      confirm_password: "",
+      address: ""
+    })
+
+    const register = (e) => {
+      // setRegBtnState("is-loading")
+      e.preventDefault()
+      //console.log(registerInfo)
+      axios
+        .post(registerURL, registerInfo)
+        .then((response) => {
+          //console.log(response)
+          if (response.data.message == "Profile created successfully") {
+            // toast.success(`${response.data.message}. Please login to continue`)
+            // setRegBtnState("")
+            setRegisterInfo({
+              name_first: "",
+              name_last: "",
+              email: "",
+              password: "",
+              confirm_password: "",
+              address: ""
+            })
+            // toggleRegModal()
+          }
+        })
+        .catch((error) => {
+          console.log(error)
+          // toast.error(error.response.data.message)
+          // setRegBtnState("")
+        })
+    }
+
+    const onRegChange = (e) => {
+      const value = e.target.value
+      setRegisterInfo({
+        ...registerInfo,
+        [e.target.name]: value,
+      })
+    }
+
+
     const [showPassword, setShowPassword] = useState(false);
+
+    function validateAddress(input) {
+      let regex = /\d\d\/\d\d\d/i;
+      return regex.test(input);
+    }
   
     return (
       <Flex
@@ -32,7 +91,7 @@ import {
               Sign up
             </Heading>
             <Text fontSize={'lg'} color={'gray.600'}>
-              to enjoy all of our cool features ✌️
+              To start using the booking system
             </Text>
           </Stack>
           <Box
@@ -45,25 +104,63 @@ import {
                 <Box>
                   <FormControl id="firstName" isRequired>
                     <FormLabel>First Name</FormLabel>
-                    <Input type="text" />
+                    <Input 
+                    required
+                    onChange={onRegChange}
+                    value={registerInfo.name_first}
+                    name='name_first'
+                    type='text'
+                    placeholder='John'
+                    />
                   </FormControl>
                 </Box>
                 <Box>
-                  <FormControl id="lastName">
+                  <FormControl id="lastName" isRequired>
                     <FormLabel>Last Name</FormLabel>
-                    <Input type="text" />
+                    <Input
+                    required
+                    onChange={onRegChange}
+                    value={registerInfo.name_last}
+                    name='name_last'
+                    type='text'
+                    placeholder='Smith'
+                    />
                   </FormControl>
                 </Box>
               </HStack>
               <FormControl id="email" isRequired>
                 <FormLabel>Email address</FormLabel>
-                <Input type="email" />
+                <Input 
+                required
+                onChange={onRegChange}
+                value={registerInfo.email}
+                name='email'
+                type='email'
+                placeholder='example@email.com'
+                />
+              </FormControl>
+              <FormControl id="address" isRequired>
+                <FormLabel>Condo / Apt Number</FormLabel>
+                <Input 
+                required
+                onChange={onRegChange}
+                value={registerInfo.address}
+                name='address'
+                type='text'
+                placeholder='88/123'
+                />
               </FormControl>
               <FormControl id="password" isRequired>
                 <FormLabel>Password</FormLabel>
                 <InputGroup>
-                  <Input type={showPassword ? 'text' : 'password'} />
-                  <InputRightElement h={'full'}>
+                  <Input 
+                  name='password' 
+                  required
+                  onChange={onRegChange}
+                  value={registerInfo.password}
+                  
+                  type={showPassword ? 'text' : 'password'} />
+                  {/* <InputRightElement h={'full'}>
                     <Button
                       variant={'ghost'}
                       onClick={() =>
@@ -71,11 +168,31 @@ import {
                       }>
                       {showPassword ? <ViewIcon /> : <ViewOffIcon />}
                     </Button>
-                  </InputRightElement>
+                  </InputRightElement> */}
+                </InputGroup>
+              </FormControl>
+              <FormControl id="confirm_password" isRequired>
+                <FormLabel>Confirm Password</FormLabel>
+                <InputGroup>
+                  <Input 
+                  required
+                  onChange={onRegChange}
+                  value={registerInfo.confirm_password}
+                  name='confirm_password' type={showPassword ? 'text' : 'password'} />
+                  {/* <InputRightElement h={'full'}>
+                    <Button
+                      variant={'ghost'}
+                      onClick={() =>
+                        setShowPassword((showPassword) => !showPassword)
+                      }>
+                      {showPassword ? <ViewIcon /> : <ViewOffIcon />}
+                    </Button>
+                  </InputRightElement> */}
                 </InputGroup>
               </FormControl>
               <Stack spacing={10} pt={2}>
                 <Button
+                  onClick={register}
                   loadingText="Submitting"
                   size="lg"
                   bg={'blue.400'}
