947a1908e407db41776147a221799e38c41628de
kn8a <mydreamhomefl@gmail.com>
2022-11-30 06:35:41 -0500

    remove unused imports and code. prettier.

--- src/App.js
+++ src/App.js
@@ -8,21 +8,10 @@ import Menu from './pages/Menu';
 import {
   ChakraProvider,
   Box,
-  Text,
-  Link,
-  VStack,
-  Code,
-  Grid,
   theme,
 } from '@chakra-ui/react';
-import { ColorModeSwitcher } from './ColorModeSwitcher';
-import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
-import {
-  BrowserRouter as Router,
-  Routes,
-  Route,
-} from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Howto from './pages/Howto';
 import axios from 'axios';
 import AdminMenu from './admin-pages/AdminMenu';
@@ -36,10 +25,8 @@ import ViewLogs from './admin-pages/ViewLogs';
 import GuardPage from './pages/GuardPage';
 
 function App() {
-
   const balanceURL = `${process.env.REACT_APP_API_URL}/users/balance`;
 
-  
   const [loggedIn, setLoggedIn] = useState({
     id: localStorage.getItem('waterfordUserId'),
     name_first: localStorage.getItem('waterfordFirstName'),
@@ -48,7 +35,7 @@ function App() {
   });
 
   useEffect(() => {
-    updateBalance()
+    updateBalance();
   }, []);
 
   const setLogin = credentials => {
@@ -56,23 +43,25 @@ function App() {
     console.log(credentials);
   };
 
-  const updateBalance = (amount) => {
-    axios.get(balanceURL, {
-      headers: { Authorization: `Bearer ${loggedIn.token}` }})
-    .then(response => {
-      setLoggedIn({...loggedIn, balance: response.data.balance.balance})
-      console.log(response.data.balance.balance)
-    })
+  const updateBalance = amount => {
+    axios
+      .get(balanceURL, {
+        headers: { Authorization: `Bearer ${loggedIn.token}` },
+      })
+      .then(response => {
+        setLoggedIn({ ...loggedIn, balance: response.data.balance.balance });
+        console.log(response.data.balance.balance);
+      });
   };
 
-  const logout = () =>{
+  const logout = () => {
     if (localStorage.getItem('waterfordUserId')) {
-      localStorage.removeItem('waterfordUserId')
-      localStorage.removeItem('waterfordFirstName')
-      localStorage.removeItem('waterfordLastName')
-      localStorage.removeItem('waterfordToken')
+      localStorage.removeItem('waterfordUserId');
+      localStorage.removeItem('waterfordFirstName');
+      localStorage.removeItem('waterfordLastName');
+      localStorage.removeItem('waterfordToken');
     }
-  }
+  };
 
   return (
     <>
@@ -82,16 +71,19 @@ function App() {
             <Routes>
               <Route
                 path="/"
-                element={<Menu loggedIn={loggedIn} setLogin={setLogin} logout={logout} />}
+                element={
+                  <Menu
+                    loggedIn={loggedIn}
+                    setLogin={setLogin}
+                    logout={logout}
+                  />
+                }
               />
               <Route
                 path="/login"
                 element={<Login loggedIn={loggedIn} setLogin={setLogin} />}
               />
-              <Route
-                path="/howto"
-                element={<Howto/>}
-              />
+              <Route path="/howto" element={<Howto />} />
               <Route
                 path="/register"
                 element={<Register loggedIn={loggedIn} />}
@@ -101,14 +93,27 @@ function App() {
                 path="/account"
                 element={<Account loggedIn={loggedIn} />}
               />
-              <Route path="/book" element={<Book loggedIn={loggedIn} updateBalance={updateBalance}/>} />
+              <Route
+                path="/book"
+                element={
+                  <Book loggedIn={loggedIn} updateBalance={updateBalance} />
+                }
+              />
               <Route
                 path="/bookings"
-                element={<Bookings loggedIn={loggedIn} updateBalance={updateBalance}/>}
+                element={
+                  <Bookings loggedIn={loggedIn} updateBalance={updateBalance} />
+                }
               />
               <Route
                 path="/admin"
-                element={<AdminMenu loggedIn={loggedIn} setLogin={setLogin} logout={logout} />}
+                element={
+                  <AdminMenu
+                    loggedIn={loggedIn}
+                    setLogin={setLogin}
+                    logout={logout}
+                  />
+                }
               />
               <Route
                 path="/admin/invite"
@@ -120,11 +125,15 @@ function App() {
               />
               <Route
                 path="/admin/past-bookings"
-                element={<PastBookings loggedIn={loggedIn} setLogin={setLogin} />}
+                element={
+                  <PastBookings loggedIn={loggedIn} setLogin={setLogin} />
+                }
               />
               <Route
                 path="/admin/future-bookings"
-                element={<FutureBookings loggedIn={loggedIn} setLogin={setLogin} />}
+                element={
+                  <FutureBookings loggedIn={loggedIn} setLogin={setLogin} />
+                }
               />
               <Route
                 path="/admin/add-user"
@@ -132,7 +141,9 @@ function App() {
               />
               <Route
                 path="/admin/manage-users"
-                element={<ManageUsers loggedIn={loggedIn} setLogin={setLogin} />}
+                element={
+                  <ManageUsers loggedIn={loggedIn} setLogin={setLogin} />
+                }
               />
               <Route
                 path="/admin/logs"
@@ -140,7 +151,13 @@ function App() {
               />
               <Route
                 path="/guard"
-                element={<GuardPage loggedIn={loggedIn} setLogin={setLogin} logout={logout} />}
+                element={
+                  <GuardPage
+                    loggedIn={loggedIn}
+                    setLogin={setLogin}
+                    logout={logout}
+                  />
+                }
               />
             </Routes>
           </Router>
