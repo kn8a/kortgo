036f7532b4ea8d8fc2b3e3f4e9fde3e8d48beee2
kn8a <mydreamhomefl@gmail.com>
2022-11-30 06:18:44 -0500

    remove unused imports and code. prettier.

--- src/admin-pages/components/Log.js
+++ src/admin-pages/components/Log.js
@@ -1,60 +1,36 @@
 import React, { useEffect, useState } from 'react';
 
-import {
-  useColorModeValue,
-  Flex,
-  Text,
-  Button,
-  Drawer,
-  DrawerBody,
-  DrawerCloseButton,
-  DrawerOverlay,
-  DrawerContent,
-  DrawerHeader,
-  DrawerFooter,
-  useDisclosure,
-  useToast,
-  FormControl,
-  Input,
-  FormLabel,
-  Textarea,
-  Select,
-  Divider,
-} from '@chakra-ui/react';
+import { useColorModeValue, Flex, Text, Divider } from '@chakra-ui/react';
 import axios from 'axios';
 import { DateTime } from 'luxon';
-import { useNavigate } from 'react-router';
-import { FaEdit } from 'react-icons/fa';
 
 function Log(props) {
-    const [color, setColor] = useState('')
-    useEffect(()=>{
-        switch (props.log.type) {
-            case 'booking':
-                setColor('green')
-                break;
-            case 'topup':
-                setColor('yellow')
-                break
-            case 'registration':
-                setColor('cyan')
-                break
-            case 'edit':
-                setColor('purple')
-                break
-            case 'refund':
-                setColor('red')
-                break
-            case 'check-in':
-                setColor('blue')
-                break;
-            
-        }
-    })
+  const [color, setColor] = useState('');
+  useEffect(() => {
+    switch (props.log.type) {
+      case 'booking':
+        setColor('green');
+        break;
+      case 'topup':
+        setColor('yellow');
+        break;
+      case 'registration':
+        setColor('cyan');
+        break;
+      case 'edit':
+        setColor('purple');
+        break;
+      case 'refund':
+        setColor('red');
+        break;
+      case 'check-in':
+        setColor('blue');
+        break;
+    }
+  });
   return (
     <div>
-
-    <Flex
+      <Flex
         rounded={'lg'}
         bg={useColorModeValue(`${color}.100`, `${color}.700`)}
         boxShadow={'md'}
@@ -65,23 +41,21 @@ function Log(props) {
         gap={2}
       >
         <Flex gap={4}>
-      <Text fontSize={'sm'}>{props.log.text}</Text>
-      <Flex flexDirection={'column'}>
-        <Text fontSize={'sm'} textAlign='right'>
-            {DateTime.fromISO(props.log.createdAt).toLocaleString(
-            DateTime.DATE_SHORT
-            )}
-        </Text>
-        <Text fontSize={'sm'} textAlign='right'>
-            {props.log.type}
-        </Text>
+          <Text fontSize={'sm'}>{props.log.text}</Text>
+          <Flex flexDirection={'column'}>
+            <Text fontSize={'sm'} textAlign="right">
+              {DateTime.fromISO(props.log.createdAt).toLocaleString(
+                DateTime.DATE_SHORT
+              )}
+            </Text>
+            <Text fontSize={'sm'} textAlign="right">
+              {props.log.type}
+            </Text>
+          </Flex>
+        </Flex>
       </Flex>
-      
-    </Flex>
-      </Flex>
-    
-    
-    <Divider/>
+
+      <Divider />
     </div>
   );
 }
